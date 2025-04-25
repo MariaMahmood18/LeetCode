be7b314787b03932016f82fe3d5e23f57f4cf557
MariaMahmood18 <138898543+MariaMahmood18@users.noreply.github.com>
2025-04-25 19:23:37 +0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (84.23%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -10,37 +10,37 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         
-        # # Strategy no.1
+        # Strategy no.1
 
-        # # Create two pointers p and q, pointing at head node.
-        # p = head # moves 2 nodes at a time
-        # q = head # moves 1 node at a time
+        # Create two pointers p and q, pointing at head node.
+        p = head # moves 2 nodes at a time
+        q = head # moves 1 node at a time
 
-        # # Traverse the list until q reaches end
-        # # Keep going while there are at least 2 nodes ahead.
-        # # This prevents errors when q tries to jump two steps (q = q.next.next).
-        # while q and q.next:
-        #     p = p.next
-        #     q = q.next.next
-        # return p
+        # Traverse the list until q reaches end
+        # Keep going while there are at least 2 nodes ahead.
+        # This prevents errors when q tries to jump two steps (q = q.next.next).
+        while q and q.next:
+            p = p.next
+            q = q.next.next
+        return p
 
 
-        # Strategy no.2
+        # # Strategy no.2
 
-        # First pass: Get the length of linked list
-        count = 0
-        current = head
-        while current:
-            count += 1
-            current = current.next
+        # # First pass: Get the length of linked list
+        # count = 0
+        # current = head
+        # while current:
+        #     count += 1
+        #     current = current.next
 
-        # Second pass: Go to second middle node
-        mid = count // 2
-        current = head
-        for _ in range(mid):
-            current = current.next
+        # # Second pass: Go to second middle node
+        # mid = count // 2
+        # current = head
+        # for _ in range(mid):
+        #     current = current.next
 
-        return current
+        # return current
 
         
 
