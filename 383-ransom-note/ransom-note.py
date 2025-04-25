3299660af1ea3527a6821a1c439eaf8619f00419
MariaMahmood18 <138898543+MariaMahmood18@users.noreply.github.com>
2025-04-25 20:07:33 +0500

    Time: 27 ms (73.01%) | Memory: 12.6 MB (97.55%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -8,32 +8,32 @@ class Solution(object):
         :rtype: bool
         """
 
-        # # Strategy no. 1
-        # magazine_dict = {} # To store the magazine letters against its occurence
+        # Strategy no. 1
+        magazine_dict = {} # To store the magazine letters against its occurence
 
-        # for letter in magazine:
-        #     if letter in magazine_dict:  # Check if the letter already exists in the dictionary
-        #         magazine_dict[letter] += 1 
-        #     else: 
-        #         magazine_dict[letter] = 1  # add new item to dictionary
-
-        # for letter in ransomNote:
-        #     if magazine_dict.get(letter, 0) == 0:
-        #         return False
-        #     else:
-        #         magazine_dict[letter] -= 1
-        # return True
-
-
-        # Strategy no.2
-        magazine_counter = Counter(magazine)
+        for letter in magazine:
+            if letter in magazine_dict:  # Check if the letter already exists in the dictionary
+                magazine_dict[letter] += 1 
+            else: 
+                magazine_dict[letter] = 1  # add new item to dictionary
 
         for letter in ransomNote:
-            if magazine_counter[letter] == 0:
+            if magazine_dict.get(letter, 0) == 0:
                 return False
             else:
-                magazine_counter[letter] -= 1
-
+                magazine_dict[letter] -= 1
         return True
+
+
+        # # Strategy no.2
+        # magazine_counter = Counter(magazine)
+
+        # for letter in ransomNote:
+        #     if magazine_counter[letter] == 0:
+        #         return False
+        #     else:
+        #         magazine_counter[letter] -= 1
+
+        # return True
 
         
